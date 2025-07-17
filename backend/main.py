b44f3a8a423f3b98c3e87405b5fa8852a494bdce
Satvik884 <satvikvinoth@gmail.com>
2025-07-17 23:41:40 +0530

    l

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["http://localhost:3000","https://ml-dashboard-wheat.vercel.app","https://*.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
